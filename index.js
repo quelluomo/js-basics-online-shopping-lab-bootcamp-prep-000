51a8dcf909b6f75734b90c8ae1724589a8ac984d
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 10:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   }
   else {
+    var finalCost = total;
     cart.length = 0;
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${finalCost()}, which will be charged to the card ${cardNumber}.`
   }
 }
