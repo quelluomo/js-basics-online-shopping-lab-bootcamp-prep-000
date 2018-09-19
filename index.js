20ae722250fab4a7edca59dbc411dc99be6dde6d
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 09:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 
 function removeFromCart(item) {
   for ( let i=0; i < cart.length; i++) {
-    if (cart[i] === [i]){
+    if (cart[i].itemName === item){
   cart.splice(i,1);
   return cart;
     }
