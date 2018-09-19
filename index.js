c65f41c1888306edd5ea061c8eae67c75cb28f78
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 09:11:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function total() {
 function removeFromCart(item) {
   for ( let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item){
-    cart.splice(i,1);
+    cart.splice(i, 1);
   return cart;
     }
   else
-  return 'That item is not in your cart.'
+  return 'That item is not in your cart.';
   }
 }
 
