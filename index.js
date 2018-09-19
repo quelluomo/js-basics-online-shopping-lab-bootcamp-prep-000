b9eeb60c16def477dd98ca2d0d16ba5739a923a7
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 09:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,15 +47,26 @@ function total() {
 
 function removeFromCart(item) {
   for ( let i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item){
-    cart.splice(i, 1);
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
   return cart;
     }
   else
   return 'That item is not in your cart.';
-  }
+  } 
 }
-
+/*
+function removeFromCart(item) {
+  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return 'That item is not in your cart.';
+}
+*/
 function placeOrder(cardNumber) {
   // write your code here
 }
