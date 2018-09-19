8bcf7d9cfcdea85472052484a042c2659955adc5
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 09:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
   }
   else {
     cart.length = 0;
-    return `Your total cost is $$
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
