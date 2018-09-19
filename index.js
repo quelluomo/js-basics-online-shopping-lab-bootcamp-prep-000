f7d1de8d87b32d3de932861d5bf2c277f76d0bab
Matteo Muscas <thatman.j@gmail.com>
2018-09-19 09:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    return 
+    return `Sorry, we don't have a credit card on file for you.`;
   }
 }
